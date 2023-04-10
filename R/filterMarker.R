1b358789a5cb46cd3b6d3d4425327c042bdb2820
JerryZhang-1222 <fyzhang.neu@gmail.com>
2023-04-10 13:46:12 +0800

    fixbug_filtermarker

--- R/filterMarker.R
+++ R/filterMarker.R
@@ -10,38 +10,52 @@
 #'
 #' @examples \dontrun{filterMarker(x = pbmc_small, ident.use = "RNA_snn_res.1", mat = diff.wilcox)}
 filterMarker <- function(x,ident.use,mat,num = 4){
-
-  ref <- x@meta.data[,ident.use] %>% table() %>% as.data.frame()
-  colnames(ref) <- c("ident_use","count")
-  rownames(ref) <- ref[,"ident_use"]
-  ref[,"ident_use"] <- NULL
-
+  ref <- x@meta.data[, ident.use] %>% table() %>% as.data.frame()
+  colnames(ref) <- c("ident_use", "count")
+  rownames(ref) <- ref[, "ident_use"]
+  ref[, "ident_use"] <- NULL
   order <- c()
-  for(i in 1:nrow(mat)){
+  for (i in 1:nrow(mat)) {
     line <- mat[i, ,drop = T]
     ident <- line$cluster %>% as.character()
-    ident.num <- ref[ident,"count"] %>% as.numeric()
+    ident.num <- ref[ident, "count"] %>% as.numeric()
     rest.num <- as.numeric(sum(ref$count) - ident.num)
     pct1 <- line["pct.1"] %>% as.numeric()
     pct2 <- line["pct.2"] %>% as.numeric()
-    rate.i <- (ident.num*pct1)/(ident.num*pct1 + rest.num*pct2)
-    order <- append(order,rate.i)
+    rate.i <- (ident.num * pct1)/(ident.num * pct1 + rest.num *
+                                    pct2)
+    order <- append(order, rate.i)
+  }
+  mat$pct.pos <- order
+  mat <- mat[order(mat[, "cluster"], -mat[, "pct.pos"]),]
+
+  mat <- mat[!duplicated(mat$gene),] %>% arrange(cluster,desc(avg_log2FC))
+
+  mat <- mat %>%
+    group_by(cluster) %>% #按照cluster列进行分组
+    top_n(num, wt = avg_log2FC) %>% #按照FC列从大到小排序，选择每组前2行
+    ungroup()
+
+  #message:
+  info <- mat$cluster %>% table() %>% as.data.frame()
+  info$echo <- dplyr::case_when(info$Freq == num ~ "Yes",
+                                info$Freq != num ~ "NO")
+  info.yes <- paste(subset(info,info$echo == "Yes")[,1],collapse = ",")
+  info.no <- paste(subset(info,info$echo == "NO")[,1],collapse = ",")
+  info.no.num <- paste(subset(info,info$echo == "NO")[,2],collapse = ",")
+  if(info.yes == ""){
+    warning(paste0("We found no cluster with ", num, " marker genes in each, please make sure you have the correct input data or try to assign a smaller num"))
+  } else {
+    message(paste0("We found ",num," marker genes for cluster: ",info.yes))
+  }
+  if(info.no != ""){
+    message(paste0("starTracer failed to find ", num," marker genes in cluster: ",info.no,", instead, we found ", info.no.num, "  marker genes in such cluster(s), respectively"))
   }
 
-  mat$trace_order <- order
-  mat <- mat[order(mat[,"cluster"],-mat[,"trace_order"]),]
-
-
-  genes.markers <- with(mat,
-                        by(mat[,"gene"],
-                           cluster,function(x){head(x,num)})) %>% unlist() %>% as.character() %>% rev()
-
+  genes.markers <- mat$gene
   p <- HeatPlot(x = x, assay = "RNA", genes = genes.markers)
 
-  filtermarkers.out <- list(
-    marker.frame = mat,
-    heatmap = p
-  )
+  filtermarkers.out <- list(marker.frame = mat, heatmap = p)
 
   return(filtermarkers.out)
 }
