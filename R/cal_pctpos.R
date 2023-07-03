0bc81280af9791fb83baf4cb466e1f95555dd41a
JerryZhang-1222 <fyzhang.neu@gmail.com>
2023-07-03 18:31:08 +0800

    fixbug_filterMarker

--- R/cal_pctpos.R
+++ R/cal_pctpos.R
@@ -21,21 +21,28 @@ cal_pctpos <- function(x,ident.use,mat,thresh.min = 0){
 
   ident_sym <- sym(ident.use)
 
+  message("getting dgCMatrix from object")
   .tmp <- as.data.frame(t(as.matrix(data[which(rownames(data) %in% mat$gene),])))
   .tmp <- rownames_to_column(.tmp,"cell.id")
   .tmp <- left_join(.tmp,meta, by = "cell.id")
+
+  message("calculating gathering result")
   .tmp <- gather(.tmp, key = "gene", value = "nUMI", c(-cell.id, -!!ident_sym))
+
+  message("calculating expression info")
   .tmp <- .tmp %>% group_by(!!ident_sym, gene) %>%
     dplyr::summarise(non.expressing.cells = sum(nUMI == 0), expressing.cells = sum(nUMI > 0)) %>%
     ungroup()
 
-  .tmp <- subset(.tmp,paste(.tmp$gene,.tmp[,ident.use],sep = "_") %in% paste(mat$gene,mat$cluster,sep = "_"))
-  .tmp$expressing.cells.all <- rowSums(data[.tmp$gene, ,drop = FALSE] > thresh.min)
+  .ref <- as.data.frame(rowSums(data[, ,drop = FALSE] > thresh.min)) %>% rownames_to_column("gene")
+  colnames(.ref)[2] <- "expressing.cells.all"
+
+  .tmp <- merge(.tmp,.ref,by = "gene")
 
   .tmp <- mutate(.tmp,pct.pos = expressing.cells/expressing.cells.all)
   .tmp <- .tmp[match(mat$gene,.tmp$gene),]
 
-  mat$pct.pos <- .tmp$pct.pos
+  mat$pct.pos <- round(.tmp$pct.pos,digits = 3)
   return(mat)
 }
 
